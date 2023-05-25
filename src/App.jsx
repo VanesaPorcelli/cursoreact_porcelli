ca5f0838c1ec73a12eb0e83288bcc5635642ea35
Vanesa Porcelli <vanesaporcelli@gmail.com>
2023-05-25 17:02:22 -0300

    primera pre entrega

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./Footer.jsx";
+import  Home  from "./Home.jsx";
+import { Navbar } from "./Navbar.jsx";
+
+
+const App = () => {
+  // logica
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
